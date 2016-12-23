9c3ac2e546c45b69f30392be7051e4354c448d00
tpalino <tpalino@linkedin.com>
2016-12-23 12:06:03 -0800

    Remove extraneous code from day 12

--- 2016/12/assembunny2.py
+++ 2016/12/assembunny2.py
@@ -1,27 +1,7 @@
 #!/usr/bin/python
 
-import re
 import sys
 
-marker_re = re.compile("\(([0-9]+)x([0-9]+)\)")
-
-def get_str_length(str):
-    str_length = 0
-    remainder = str
-
-    while remainder != '':
-        m = marker_re.search(remainder)
-        if m:
-            str_length += m.start()
-
-            repeat_len = int(m.group(1))
-            uncompressed_length, junk = get_str_length(remainder[m.end():m.end()+repeat_len])
-            str_length += uncompressed_length * int(m.group(2))
-            remainder = remainder[m.end()+repeat_len:]
-        else:
-            return len(remainder), ''
-    return str_length, remainder[m.end()+repeat_len:]
-    
 fn = sys.argv[1]
 fh = open(fn, "r")
 
