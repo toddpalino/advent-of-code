2fcc47bc328c672a70cae482a949ba6a0e7c6c50
Todd Palino <github@bonkoif.com>
2024-12-05 21:51:04 -1000

    Adding a template script to use

---
+++ template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import time
+
+fn = "test.txt"
+#fn = "input.txt"
+
+start_time = time.time()
+
+with open(fn, 'r') as f:
+	pass
+
+end_time = time.time()
+print("Elapsed time: %f" % (end_time - start_time))
