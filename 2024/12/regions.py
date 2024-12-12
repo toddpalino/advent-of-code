523237c6422d666831cada66b9adaa3062587374
Todd Palino <github@bonkoif.com>
2024-12-11 20:45:56 -1000

    Small perf improvement to the logic to separate out regions

--- 2024/12/regions.py
+++ 2024/12/regions.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 
 from collections import deque
+from itertools import product
 
 def make_regions(grid):
-	len_x = len(grid[0])
-	len_y = len(grid)
 	regions = []
 
 	# Put all the coords into a set which we'll use to figure out what we haven't looked at
-	coords = set((x, y) for x in range(len_x) for y in range(len_y))
+	coords = set(product(range(len(grid[0])), range(len(grid))))
 
 	while coords:
-		# Pull any coord from the set
+		# Pull any coord from the set to start a region
 		c = coords.pop()
+		r = set([c])
 		l = grid[c[1]][c[0]]
 
-		# Create an empty region
-		r = set([c])
+		# Look at neighbors to build this region
 		queue = deque([c])
 		while queue:
 			x, y = queue.popleft()
-			for nx, ny in ((x-1, y), (x+1, y), (x, y-1), (x, y+1)):
-				if not ((0 <= nx < len_x) and (0 <= ny < len_y)):
-					continue
-				n = (nx, ny)
+			for n in ((x-1, y), (x+1, y), (x, y-1), (x, y+1)):
 				if n not in coords:
 					continue
-				if grid[ny][nx] == l:
+				if grid[n[1]][n[0]] == l:
 					# This adjacent plot is in the same region
 					coords.remove(n)
 					r.add(n)
